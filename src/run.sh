e7ea70765416b12759e48914495bc4f888f35f37
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2011-05-03 16:33:45 +0530

    Made run wrapper more uniform

--- src/run.sh
+++ src/run.sh
@@ -1,11 +1,11 @@
 # Run 
 
-AGENT=IntraOptionQ
+AGENT=MacroQ
 TRIALS=200
 EPISODES=1500
 GAMMA=0.99 #0.99
 RATE=0.01 #0.01
-SCHEMES="small-world betweenness manual none random optimal" #none
+SCHEMES="manual optimal" #none
 DOMAIN="taxi1.txt"
 
 alpha=0.8
@@ -17,7 +17,7 @@ for gamma in $GAMMA; do
             for domain in $DOMAIN; do
                 echo "# Running with g,r,s,d = $gamma, $rate, $scheme, $domain"
                 cmd="python2 ./main.py $EPISODES "$AGENT:$gamma:$alpha:$e:$rate" "TaxiOptions:./data/$domain:$scheme" "
-                out="./intraQ/$(basename $domain .txt)-$scheme-$rate-$gamma"
+                out="./${AGENT}-${gamma}/$(basename $domain .txt)-$scheme-$rate-$gamma"
                 if [ ! -e $out ]; then mkdir $out; fi;
                 for i in `seq 1 $TRIALS`; do
                     echo $(calc "round($i/$TRIALS * 100)")
