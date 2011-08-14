5969d2b8234eb689dd09103dc418c59501f5076d
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2011-08-14 17:09:52 +0530

    Added an option count to the run scripts

--- src/run.sh
+++ src/run.sh
@@ -1,27 +1,31 @@
 # Run 
 
-AGENT=MacroQ
-TRIALS=200
-EPISODES=1500
+AGENT=IntraOptionQ
+TRIALS=500
+EPISODES=2000
 GAMMA=0.99 #0.99
 RATE=0.01 #0.01
-SCHEMES="manual optimal" #none
+SCHEMES="small-world betweenness none random ozgur-random optimal" #none
 DOMAIN="taxi1.txt"
+OCount=20
 
 alpha=0.8
 e=0.1
+
+SD=./scripts
 
 for gamma in $GAMMA; do
     for rate in $RATE; do
         for scheme in $SCHEMES; do
             for domain in $DOMAIN; do
                 echo "# Running with g,r,s,d = $gamma, $rate, $scheme, $domain"
-                cmd="python2 ./main.py $EPISODES "$AGENT:$gamma:$alpha:$e:$rate" "TaxiOptions:./data/$domain:$scheme" "
-                out="./${AGENT}-${gamma}/$(basename $domain .txt)-$scheme-$rate-$gamma"
+                cmd="python2 ./main.py $EPISODES "$AGENT:$gamma:$alpha:$e:$rate" "TaxiOptions:./data/$domain:$scheme:$OCount" "
+                out="output-$OCount/${AGENT}-${gamma}/$(basename $domain .txt)-$scheme-$rate-$gamma"
                 if [ ! -e $out ]; then mkdir $out; fi;
                 for i in `seq 1 $TRIALS`; do
                     echo $(calc "round($i/$TRIALS * 100)")
                     $cmd > $out/$i.dat;
+                    $SD/add_x -w $out/$i.dat;
                 done;
             done;
         done;
